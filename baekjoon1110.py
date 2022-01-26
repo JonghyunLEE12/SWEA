3b0709f0d80c6c1f29c743e6b5ebfd20ee6ef663
Jonghyun <najo7609@gmail.com>
2022-01-26 23:37:42 +0900

    0126

--- baekjoon1110.py
+++ baekjoon1110.py
@@ -1,17 +1,22 @@
-#baekjoon1110
-from turtle import numinput
-
 
 N = int(input())
 
+#number 변수에 N을 저장
 number = N
+#cnt 변수 선언
 cnt = 0
 while True:
-    a = number //10
-    b = number % 10
-    c = (a+b) % 10
-    number = (b*10) + c
+    #26
+    # a 값에 number를 10으로 나눈 몫을 저장
+    a = number //10 #2
+    # b 값에 number의 10으로 나눈 나머지 저장
+    b = number % 10  #6
+    # c 값에 number의 10의 자리수와 1의 자리수를 저장후 10으로 나눈 나머지값 저장
+    c = (a+b) % 10 #8
+    # number에 새로운 값을 대입
+    number = (b*10) + c #68
     cnt += 1
+    # number 의 값과 N의 값이 똑같을 때, while문을 빠져나옴
     if number == N:
         break
 print(cnt)
--- baekjoon2798.py
+++ baekjoon2798.py
@@ -6,31 +6,19 @@ N , M = map(int,input().split())
 #카드의 리스트가 나옴
 card_list= list(map(int,input().split()))
 
-# def black_jack(card_list):
-#     result_total=[]
-#     for number1 in range(N):
-#         for number2 in range(number1 , N):
-#             for number3 in range(number2,N):
-#                 total = card_list[number1] + card_list[number2] + card_list[number3] 
-#                 if total > M:
-#                     continue
-#                 else :
-#                     result_total.append(total)
-#                     result = max(result_total)
-#     return result
+def black_jack(card_list):
+    result_total=[]
+    for number1 in range(N):
+        for number2 in range(number1 , N):
+            for number3 in range(number2,N):
+                total = card_list[number1] + card_list[number2] + card_list[number3] 
+                if total > M:
+                    continue
+                else :
+                    result_total.append(total)
+                    result = max(result_total)
+    return result
 
-# print(black_jack(card_list))
+print(black_jack(card_list))
 
 
-result_total=[]
-for number1 in range(N):
-    for number2 in range(number1 , N):
-        for number3 in range(number2,N):
-            total = card_list[number1] + card_list[number2] + card_list[number3] 
-            if total > M:
-                continue
-            else :
-                result_total.append(total)
-                result = max(result_total)
-
-print(result)
