77a5bb108acb4435a2b6bde5bf4296c7e42ed96d
Jonghyun <najo7609@gmail.com>
2022-02-01 21:52:52 +0900

    0201

--- baekjoon2491.py
+++ baekjoon2491.py
@@ -1,15 +1,49 @@
+# N = int(input())
+# numbers = list(map(int,input().split()))
+#
+# cnt = 1
+# max_cnt = 1
+# for i in range(len(numbers)-1):
+#     if numbers[i] <= numbers[i+1]:
+#         cnt += 1
+#     else :
+#         cnt = 1
+#     if max_cnt < cnt:
+#         max_cnt = cnt
+#
+# for i in range(len(numbers)-1):
+#     if numbers[i] >= numbers[i+1]:
+#         cnt += 1
+#     else :
+#         cnt = 1
+#     if max_cnt < cnt:
+#         max_cnt = cnt
+# print(max_cnt)
+
+
+
+
+
 N = int(input())
 numbers = list(map(int,input().split()))
 
 cnt = 1
-##커질때
-for i in range(2,len(numbers)):
-    if numbers[i-1] >= numbers[i-2]:
-        if numbers[i] >= numbers[i-1]:
-            cnt+=1
+max_cnt = 1
 
-print(cnt)
-
-
-
-
+#증가 할 때
+for i in range(N-1):
+    if numbers[i] <= numbers[i+1]:
+        cnt +=1
+    else:
+        cnt = 1
+    if cnt > max_cnt:
+        max_cnt = cnt
+#감소 할 떄
+for i in range(N-1):
+    if numbers[i] >= numbers[i+1]:
+        cnt += 1
+    else:
+        cnt = 1
+    if cnt > max_cnt:
+        max_cnt = cnt
+print(max_cnt)
