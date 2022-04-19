c6b86100fa526962c2bf3bae78923c98947e5e40
Jong Hyun LEE <59881536+JonghyunLEE12@users.noreply.github.com>
2022-04-19 22:32:35 +0900

    [Silver I] Title: 나이트의 이동, Time: 324 ms, Memory: 129092 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -1,27 +1,34 @@
 from collections import deque
-import sys
-input = sys.stdin.readline
-dx = [-1, -2, -2, -1, 1, 2, 2, 1]
-dy = [2, 1, -1, -2, -2, -1, 1, 2]
-def bfs(sx, sy, ax, ay):
-    q = deque()
-    q.append([sx, sy])
-    s[sx][sy] = 1
-    while q:
-        a, b = q.popleft()
-        if a == ax and b == ay:
-            print(s[ax][ay] -1)
+
+# 델타
+dr = [-1, -2, -2, -1, 1, 2, 2, 1]
+dc = [2, 1, -1, -2, -2, -1, 1, 2]
+
+
+def bfs(sr,sc,gr,gc):
+    queue = deque()
+    queue.append((sr,sc))
+    matrix[sr][sc] = 1
+    while queue:
+        r,c = queue.popleft()
+        if r == gr and c == gc:
+            print(matrix[gr][gc]-1)
             return
-        for i in range(8):
-            x = a + dx[i]
-            y = b + dy[i]
-            if 0 <= x < n and 0 <= y < n and s[x][y] == 0:
-                q.append([x, y])
-                s[x][y] = s[a][b] + 1
-t = int(input())
-for i in range(t):
-    n = int(input())
-    sx, sy = map(int, input().split())
-    ax, ay = map(int, input().split())
-    s = [[0] * n for i in range(n)]
-    bfs(sx, sy, ax, ay)
+        for i in range(len(dr)):
+            nr = r + dr[i]
+            nc = c + dc[i]
+
+            if 0 <= nr < l and 0 <= nc < l and matrix[nr][nc] == 0 :
+                queue.append((nr,nc))
+                matrix[nr][nc] = matrix[r][c] + 1
+
+
+
+tc = int(input())
+for _ in range(tc):
+    l = int(input())
+    matrix = [[0]*l for _ in range(l)]
+    start_r,start_c = map(int,input().split())
+    goal_r, goal_c = map(int,input().split())
+
+    bfs(start_r,start_c,goal_r,goal_c)
