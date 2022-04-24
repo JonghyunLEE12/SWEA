f883492870f05403491f59235c50c572054f0a03
Jong Hyun LEE <59881536+JonghyunLEE12@users.noreply.github.com>
2022-04-24 18:08:43 +0900

    [Silver III] Title: 바이러스, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,22 +1,31 @@
-# 컴퓨터의 수
-n = int(input())
-m = int(input())
-
-node = [[0]* (n+1) for i in range(n+1)]
-stack = [0] * (n+1)
-
-for _ in range(m):
-    a,b = map(int,input().split())
-    node[a][b] = node[b][a] = 1
-
-rlt = []
-
+'''
+7
+6
+1 2
+2 3
+1 5
+5 2
+5 6
+4 7
+'''
 def dfs(v):
     stack[v] = 1
-    for i in range(1,n+1):
-        if stack[i] == 0 and node[i][v] == 1:
+    for i in range(computers+1):
+        if stack[i] == 0 and nodes[i][v]:
             rlt.append(i)
             dfs(i)
 
+computers = int(input())
+m = int(input())
+
+nodes = [[0]*(computers+1) for _ in range(computers+1)]
+stack = [0]*(computers+1)
+
+for i in range(m):
+    a,b = map(int,input().split())
+    nodes[a][b] = nodes[b][a] = 1
+
+
+rlt = []
 dfs(1)
 print(len(rlt))
