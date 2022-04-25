71d22fad09a9602f8f0745eaaef5f0efd3bfe305
Jong Hyun LEE <59881536+JonghyunLEE12@users.noreply.github.com>
2022-04-25 22:40:42 +0900

    [Silver I] Title: 단지번호붙이기, Time: 88 ms, Memory: 32436 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,33 +1,45 @@
 from collections import deque
-n = int(input())
-apt = []
-for i in range(n):
-    apt.append(list(map(int, input())))			
-visit = [[0] * n for i in range(n)]				# 1 
-dx = [-1, 0, 1 ,0]						# 2
-dy = dx[::-1]							
-def bfs(x, y, idx):
-    q = deque([[x,y]])						# 6
-    visit[x][y] = 1				
-    while q:
-        node = q.popleft()
+
+# 델타 : 상하좌우
+
+dr = [-1,1,0,0]
+dc = [0,0,-1,1]
+
+# 방문처리도 있으면 좋을듯
+def bfs(row,col,apt_num):
+    queue = deque()
+    queue.append((row,col))
+    visited[row][col] = 1
+    while queue:
+        r,c = queue.popleft()
         for i in range(4):
-            nx = node[0] + dx[i]				# 7
-            ny = node[1] + dy[i]
-            if 0 <= nx < n and 0 <= ny < n:			# 8
-                if apt[nx][ny] == 1 and visit[nx][ny] == 0: 	# 9
-                    q.append([nx,ny])				# 10
-                    visit[nx][ny] = 1				# 11
-                    apt_list[idx] += 1				# 12
-apt_list={}
-idx = 0
-for i in range(n):						# 3
+            nr = r + dr[i]
+            nc = c + dc[i]
+
+            # 범위체크 한번 해주자
+            if 0 <= nr < n and 0<= nc < n:
+                if matrix[nr][nc] != 0 and visited[nr][nc] == 0:
+                    apt_list[apt_num] += 1
+                    queue.append((nr,nc))
+                    visited[nr][nc] = 1
+
+n = int(input())
+matrix = [ list(map(int,input())) for _ in range(n)]
+visited = [[0]*n for _ in range(n)]
+
+
+apt_num = 0
+apt_list = dict()
+for i in range(n):
     for j in range(n):
-        if apt[i][j] != 0 and visit[i][j] == 0:			# 4
-            apt_list[idx] = 1					# 5
-            bfs(i,j,idx)
-            idx+=1						# 13
-apt_list=sorted(apt_list.values())
+        if matrix[i][j] != 0 and visited[i][j] == 0:
+            apt_list[apt_num] = 1
+            bfs(i,j,apt_num)
+            apt_num += 1
+
+apt_list = sorted(apt_list.values())
 print(len(apt_list))
 for i in apt_list:
     print(i)
+
+
