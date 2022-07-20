51fa85976b7ff62ad16cf3a8e7c20ee9f0979512
Jong Hyun LEE <59881536+JonghyunLEE12@users.noreply.github.com>
2022-07-20 23:46:39 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -0,0 +1,13 @@
+-- 코드를 입력하세요
+SELECT hour(datetime) as HOUR, count(datetime) as COUNT
+from animal_outs
+where hour(datetime) >= 9 and hour(datetime) <= 19
+group by hour
+order by hour
+
+
+# SELECT HOUR(DATETIME) AS HOUR, COUNT(DATETIME) AS COUNT
+# FROM ANIMAL_OUTS
+# WHERE HOUR(DATETIME) >= 9 AND HOUR(DATETIME) <= 19
+# GROUP BY HOUR 
+# ORDER BY HOUR;
