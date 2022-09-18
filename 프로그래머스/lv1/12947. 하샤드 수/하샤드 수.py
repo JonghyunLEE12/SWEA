e1f895a63688a3bfb66d5d533c7f708c7977f97c
Jong Hyun LEE <59881536+JonghyunLEE12@users.noreply.github.com>
2022-09-19 01:21:24 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
@@ -0,0 +1,11 @@
+def solution(x):
+    answer = True
+    
+    str_x = str(x)
+    sum_x = 0
+    for i in str_x:
+        sum_x += int(i)
+        
+    if x % sum_x:
+        answer = False
+    return answer
