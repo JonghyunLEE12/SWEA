6353616562bc75f0a9dd8927f14908db335b9e84
Jong Hyun LEE <59881536+JonghyunLEE12@users.noreply.github.com>
2022-09-22 22:37:19 +0900

    [level 2] Title: 이름에 el이 들어가는 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++ 프로그래머스/lv2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -1,4 +1,9 @@
 -- 코드를 입력하세요
-SELECT animal_id , name from animal_ins
-where animal_type = "Dog" and name like "%el%"
-order by name
+# SELECT animal_id , name from animal_ins
+# where animal_type = "Dog" and name like "%el%"
+# order by name
+
+
+SELECT animal_id , name FROM animal_ins
+WHERE animal_type = 'Dog' AND name LIKE "%el%"
+ORDER BY name
