9f217bf107ffa626f3367c845d2a1ed5a8d43732
Jong Hyun LEE <59881536+JonghyunLEE12@users.noreply.github.com>
2022-09-23 21:28:13 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,17 @@
+# -- 코드를 입력하세요
+# SELECT animal_ins.name, animal_ins.datetime
+# from animal_ins
+# left join animal_outs
+# on animal_ins.animal_id = animal_outs.animal_id
+# where animal_outs.animal_id is null
+# order by animal_ins.datetime
+# limit 3
+
+
+SELECT animal_ins.name , animal_ins.datetime
+FROM animal_ins
+LEFT JOIN animal_outs
+ON animal_ins.animal_id = animal_outs.animal_id
+WHERE animal_outs.animal_id IS NULL
+ORDER BY animal_ins.datetime
+limit 3
