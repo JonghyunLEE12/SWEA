7623a9d57fa4d92641acf12fc22a1d965ddf11ba
Jong Hyun LEE <59881536+JonghyunLEE12@users.noreply.github.com>
2022-09-27 19:48:26 +0900

    [level 2] Title: 고양이와 개는 몇 마리 있을까, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59040. 고양이와 개는 몇 마리 있을까/고양이와 개는 몇 마리 있을까.sql
+++ 프로그래머스/lv2/59040. 고양이와 개는 몇 마리 있을까/고양이와 개는 몇 마리 있을까.sql
@@ -1,5 +1,23 @@
 -- 코드를 입력하세요
-SELECT animal_type , COUNT(animal_type)
+
+# SELECT animal_type, count(animal_type)
+# FROM animal_ins
+# GROUP BY animal_type
+# ORDER BY animal_type
+
+
+
+
+
+
+
+
+
+
+
+
+SELECT animal_type , count(animal_type)
 FROM animal_ins
 GROUP BY animal_type
 ORDER BY animal_type
+
