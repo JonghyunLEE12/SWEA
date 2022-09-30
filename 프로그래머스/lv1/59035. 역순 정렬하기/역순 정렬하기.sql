90ac1bdd6d5a6581b6ff3c76b2684ee80793a5e7
Jong Hyun LEE <59881536+JonghyunLEE12@users.noreply.github.com>
2022-09-30 17:13:21 +0900

    [level 1] Title: 역순 정렬하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv1/59035. 역순 정렬하기/역순 정렬하기.sql
+++ 프로그래머스/lv1/59035. 역순 정렬하기/역순 정렬하기.sql
@@ -1,2 +1,6 @@
 -- 코드를 입력하세요
-SELECT name ,  datetime from animal_ins order by animal_id desc
+# SELECT name ,  datetime from animal_ins order by animal_id desc
+
+
+SELECT name , datetime FROM animal_ins
+ORDER BY animal_id desc
