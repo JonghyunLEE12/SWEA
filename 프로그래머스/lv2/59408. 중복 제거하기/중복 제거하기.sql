ac8d0022a262154ada2a6ee6779c0a796737b27f
Jong Hyun LEE <59881536+JonghyunLEE12@users.noreply.github.com>
2022-09-30 17:20:00 +0900

    [level 2] Title: 중복 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59408. 중복 제거하기/중복 제거하기.sql
+++ 프로그래머스/lv2/59408. 중복 제거하기/중복 제거하기.sql
@@ -1,4 +1,10 @@
 -- 코드를 입력하세요
-SELECT count(DISTINCT name) AS count 
-FROM animal_ins
+# SELECT count(DISTINCT name) AS count 
+# FROM animal_ins
+# WHERE name IS NOT NULL
+
+
+
+
+SELECT COUNT(DISTINCT name)  FROM animal_ins
 WHERE name IS NOT NULL
