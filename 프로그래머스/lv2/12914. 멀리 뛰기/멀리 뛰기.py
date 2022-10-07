2a5b01d44487aac385f723856f4a29e4624b4aa9
Jong Hyun LEE <59881536+JonghyunLEE12@users.noreply.github.com>
2022-10-07 17:18:56 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.69 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,11 +1,10 @@
 def solution(n):
-    answer = 0
     if n < 2:
         return n
-    
     dp = [0] * n
     dp[0] = 1
     dp[1] = 2
+    
     for i in range(2,n):
         dp[i] = dp[i-1] + dp[i-2]
     
