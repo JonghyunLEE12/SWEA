ccc2dc363fe74588c4d9ce339fb1c0b5a804e787
Jong Hyun LEE <59881536+JonghyunLEE12@users.noreply.github.com>
2022-10-08 16:44:16 +0900

    [Silver I] Title: 스티커, Time: 276 ms, Memory: 166520 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -1,15 +1,16 @@
-
 T = int(input())
+
 for tc in range(T):
     n = int(input())
     matrix = [list(map(int,input().split(' '))) for _ in range(2)]
 
-    for j in range(1,n):
-        if j == 1:
-            # 대각선 의 값을 덯해 주는 과정
-            matrix[0][j] += matrix[1][j-1]
-            matrix[1][j] += matrix[0][j-1]
+    for i in range(1,n):
+        if i == 1:                                                      # i = 1 일 때, 대각전 0 열 의 수를 더해준다
+            matrix[0][i] += matrix[1][i-1]
+            matrix[1][i] += matrix[0][i-1]
         else:
-            matrix[0][j] += max(matrix[1][j-1] , matrix[1][j-2])
-            matrix[1][j] += max(matrix[0][j-1], matrix[0][j-2])
+            matrix[0][i] += max(matrix[1][i-1], matrix[1][i-2])         # 같은 변을 공유할 수는 없다, i-1 열 과 i-2열을 비교하여 최대값을 더해준다
+            matrix[1][i] += max(matrix[0][i-1], matrix[0][i-2])
+
+    
     print(max(matrix[0][n-1], matrix[1][n-1]))
