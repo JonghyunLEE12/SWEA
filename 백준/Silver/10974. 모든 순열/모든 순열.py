b80af90f1826a5120834919d1aee7f79d004b5a2
Jong Hyun LEE <59881536+JonghyunLEE12@users.noreply.github.com>
2022-10-14 17:16:37 +0900

    [Silver III] Title: 모든 순열, Time: 204 ms, Memory: 119024 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.py
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -1,16 +1,14 @@
-
-
 n = int(input())
-temp = []
 
+temp = []
 def dfs():
     if len(temp) == n:
         print(*temp)
         return
-    for i in range(1, n + 1):
+    
+    for i in range(1,n+1):
         if i not in temp:
             temp.append(i)
             dfs()
             temp.pop()
-
 dfs()
