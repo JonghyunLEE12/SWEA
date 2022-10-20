07fc539d36a982455466e192447e60b8b03fe3c9
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2022-10-20 22:33:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 156.45 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -15,5 +15,22 @@ def solution(p, c):
     p.sort()
     c.sort()
     result = collections.Counter(p) - collections.Counter(c)
+    print(list(result)[0])
     
     return list(result)[0]
+
+
+# from collections import defaultdict
+# def solution(participant,completion):
+#     answer = 0
+    
+#     n_dict = defaultdict(int)
+#     for i in participant:
+#         n_dict[i] += 1
+    
+#     for j in completion:
+#         n_dict[j]+= 1    
+    
+#     rlt = []
+#     print(n_dict[1])
+#     return answer
