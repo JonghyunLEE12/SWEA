8f3f2a2eb883e94b38955fd79afd19e77979bed6
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2022-10-31 17:05:23 +0900

    [level 2] Title: 피로도, Time: 0.14 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -1,42 +1,47 @@
-# def dfs(k,count,dungeons):
-#     for i in range(n):
-        
-
-# def solution(k, dungeons):
-#     global n
-#     answer = 0
-#     # sorted_dungeons = sorted(dungeons, key=lambda x : x[0])
-#     # print(sorted_dungeons)
-    
-#     # 모든 경우 탐색
-#     n = len(dungeons)
-#     visited = [0] * n
-#     print(visited)
-#     dfs (k,0,dungeons)
-            
-#     return answer
-
-
 answer = 0
-N = 0
 visited = []
-
-
-def dfs(k, cnt, dungeons):
-    global answer
-    if cnt > answer:
-        answer = cnt
-
-    for j in range(N):
-        if k >= dungeons[j][0] and not visited[j]:
-            visited[j] = 1
-            dfs(k - dungeons[j][1], cnt + 1, dungeons)
-            visited[j] = 0
-
+n = 0
 
 def solution(k, dungeons):
-    global N, visited
-    N = len(dungeons)
-    visited = [0] * N
-    dfs(k, 0, dungeons)
+    global n , visited
+    n = len(dungeons)
+    visited = [0] * n
+    dfs(k,0,dungeons)
+    
     return answer
+
+def dfs(k,count,dungeons):
+    global answer
+    if count > answer:
+        answer = count
+        
+    for i in range(n):
+        if k >= dungeons[i][0] and not visited[i]:
+            visited[i] = 1
+            dfs(k-dungeons[i][1] , count +1 , dungeons)
+            visited[i] = 0
+
+
+# answer = 0
+# N = 0
+# visited = []
+
+
+# def dfs(k, cnt, dungeons):
+#     global answer
+#     if cnt > answer:
+#         answer = cnt
+
+#     for j in range(N):
+#         if k >= dungeons[j][0] and not visited[j]:
+#             visited[j] = 1
+#             dfs(k - dungeons[j][1], cnt + 1, dungeons)
+#             visited[j] = 0
+
+
+# def solution(k, dungeons):
+#     global N, visited
+#     N = len(dungeons)
+#     visited = [0] * N
+#     dfs(k, 0, dungeons)
+#     return answer
