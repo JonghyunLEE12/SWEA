c30f4f2233a0548082320db148f2b643568a3782
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2022-11-10 19:36:07 +0900

    [level 1] Title: 삼총사, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,34 +1,40 @@
-function solution(number) {
-    let result = 0;
-
-    const combination = (current, start) => {
-        if (current.length === 3) {
-            result += current.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
-            return;
-        }
-
-        for (let i = start; i < number.length; i++) {
-            combination([...current, number[i]], i + 1);
-        }
-    }
-    combination([], 0);
-    return result;
-}
-
 // function solution(number) {
-//     var answer = 0;
-//     // 학교 학생 3명의 정수를 더했을 때 0 이 되면 삼총사
-    
+//     let result = 0;
+
 //     const combination = (current, start) => {
-//        if (current.length === 3) {
-//            if (current.reduce((a,b) => a+b,0) === 0) {
-//                answer += 1
-//            }
-//        }
-//         for (let i = 0 ; i < number.length ; i ++) {
-//             combination([...current, number[i]],i+1)
+//         if (current.length === 3) {
+//             if ( current.reduce((a,b)=> a+b ,0) === 0) {
+//                 result += 1
+//             }
+//             return;
+//         }
+
+//         for (let i = start; i < number.length; i++) {
+//             combination([...current, number[i]], i + 1);
 //         }
 //     }
-//     combination([],0)
-//     return answer;
+//     combination([], 0);
+//     return result;
 // }
+
+
+
+
+function solution(number) {
+    let result = 0;
+    
+    
+    const combination = (current,start) => {
+        if (current.length === 3){
+            if (current.reduce((a,b) => a+b, 0) === 0) {
+                result += 1
+            }
+            return
+        }
+        for (let i = start ; i < number.length ; i ++) {
+            combination([...current,number[i]],i+1)
+        }
+    }
+    combination([],0)
+    return result;
+}
