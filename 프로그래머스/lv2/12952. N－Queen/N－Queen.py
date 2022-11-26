ca0fa073fdf847a0acced2a12400d7951e2f80c1
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2022-11-26 18:52:59 +0900

    [level 2] Title: N-Queen, Time: 8601.68 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/lv2/12952. N－Queen/N－Queen.py
@@ -1,68 +1,42 @@
-# from collections import deque
-
-# # Queen 은 상 하 대각선으로 이동 가능
-# dr = [-1,1,0,0,1,-1,1,-1]
-# dc = [0,0,-1,1,1,-1,-1,1]
-
-# answer = 0
-# def check(row,col,matrix,visited):
-#     for i in range(1,len(matrix)):
-#         for j in range(len(dr)):
-#             nr = row + dr[j] * i
-#             nc = col + dc[j] * i
-#             if 0 <= nr < len(matrix) and 0<= nc < len(matrix):
-#                 if visited[nr][nc] == 0:
-#                     visited[nr][nc] = 1
-
-# def dfs(row,col,matrix,visited,cnt):
-#     if cnt <= len(visited):
-#         answer += 1
-#     elif cnt > len(visited):
-#         return cnt
-#     else:
-#         check(row,col,matrix,visited)
-#         for i in range(len(matrix)):
-#             dfs(cnt,i,matrix,visited,cnt+1)
-                    
+# def dfs(queen,n,row):
+#     count = 0
     
-#     return
+#     if n == row:
+#         return 1
+#     for col in range(n):
+#         queen[row] = col
+        
+#         for x in range(row):
+#             if queen[x] == queen[row]:
+#                 break
+#             if abs(queen[x] - queen[row]) == (row - x):
+#                 break
+#     else:
+#         count += dfs(queen , n , row + 1)        
+                
+#     return count
+
 
 # def solution(n):
-#     global answer
-#     matrix = [[0]*n for _ in range(n)]
-    
-#     for i in range(n):
-#         visited = [[0]*n for _ in range(n)] 
-#         visited[0][i] = 1
-#         dfs(0,i,matrix,visited,1)
+#     queen = [0]* n
+#     answer = dfs(queen,n,0)
 #     return answer
 
 
-def dfs(queen, n, row):
+
+def dfs(queen, row, n):
     count = 0
-    
     if n == row:
         return 1
-    
-    # 가로로 한번만 진행
     for col in range(n):
         queen[row] = col
-
-        # for-else구문
-        for x in range(row):
-            # 세로로 겹치면 안됨
-            if queen[x] == queen[row]: 
+        for i in range(row):
+            if queen[i] == queen[row]:
                 break
-                
-            # 대각선으로 겹치면 안됨
-            if abs(queen[x]-queen[row]) == (row-x):
+            if abs(queen[i]-queen[row]) == row - i:
                 break
         else:
-            count += dfs(queen, n, row+1)
-            
+            count += dfs(queen, row + 1, n)
     return count
-
 def solution(n):
-    queen = [0]*n
-        
-    return dfs(queen, n, 0)
+    return dfs([0]*n, 0, n)
--- 프로그래머스/lv2/12952. N－Queen/README.md
+++ 프로그래머스/lv2/12952. N－Queen/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 9079.26 ms
+메모리: 10.3 MB, 시간: 8601.68 ms
 
 ### 구분
 
@@ -20,8 +20,8 @@
 
 <p>예를 들어서 n이 4인경우 다음과 같이 퀸을 배치하면 n개의 퀸은 서로를 한번에 공격 할 수 없습니다.</p>
 
-<p><img src="https://i.imgur.com/lt2zdK6.png" title="" alt="Imgur" id="exifviewer-img-2" exifid="-1472911344" oldsrc="https://i.imgur.com/lt2zdK6.png"><br>
-<img src="https://i.imgur.com/5c5EUrq.png" title="" alt="Imgur" id="exifviewer-img-1" exifid="-1853226526" oldsrc="https://i.imgur.com/5c5EUrq.png"></p>
+<p><img src="https://i.imgur.com/lt2zdK6.png" title="" alt="Imgur"><br>
+<img src="https://i.imgur.com/5c5EUrq.png" title="" alt="Imgur"></p>
 
 <p>체스판의 가로 세로의 세로의 길이 n이 매개변수로 주어질 때, n개의 퀸이 조건에 만족 하도록 배치할 수 있는 방법의 수를 return하는 solution함수를 완성해주세요.</p>
 
