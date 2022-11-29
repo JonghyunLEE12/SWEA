06e7e1901777de0709da6dea9a059b307c9522f2
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2022-11-29 18:14:35 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,21 +1,35 @@
 function solution(ingredient) {
-    const stk = [];
+    const stack = [];
     let cnt = 0;
     
-    ingredient.forEach((ing, idx)=>{
-        stk.push(ing);  
-       
-        if(stk.length >= 4){
-            const find = stk.slice(-4).join('');
-            if(find === '1231'){
-                stk.pop();
-                stk.pop();
-                stk.pop();
-                stk.pop();   
-                cnt++;
+    ingredient.forEach((ing,idx) => {
+        
+        stack.push(ing)
+        if (stack.length >= 4) {
+            const find = stack.slice(-4).join('')
+            if (find === '1231') {
+                stack.pop()
+                stack.pop()
+                stack.pop()
+                stack.pop()
+                cnt ++
             }
-        } 
+        }
     })
+//     ingredient.forEach((ing, idx)=>{
+//         stk.push(ing);  
+       
+//         if(stk.length >= 4){
+//             const find = stk.slice(-4).join('');
+//             if(find === '1231'){
+//                 stk.pop();
+//                 stk.pop();
+//                 stk.pop();
+//                 stk.pop();   
+//                 cnt++;
+//             }
+//         } 
+//     })
     
     return cnt;
 }
