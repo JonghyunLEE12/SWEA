f402ebc1e2366c64ba2e3f1605881d59373d06a7
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2022-12-05 19:03:10 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,19 @@
+function solution(dot) {
+    var answer = 0;
+    
+    // switch(dot) {
+    //     case dot[0] > 0 && dot[1] > 0:
+    //         return 1
+    //     case dot[0] > 0 && dot[1] < 0:
+    //         return 4
+    //     case dot[0] < 0 && dot[1] > 0:
+    //         return 2
+    //     case dot[0] < 0 && dot[1] < 0:
+    //         return 3
+    // }
+    
+    if (dot[0] > 0 && dot[1] > 0 ) {return 1}
+    else if (dot[0] > 0 && dot[1] < 0) {return 4}
+    else if (dot[0] < 0 && dot[1] > 0) {return 2}
+    else if (dot[0] < 0 && dot[1] < 0) {return 3}
+}
