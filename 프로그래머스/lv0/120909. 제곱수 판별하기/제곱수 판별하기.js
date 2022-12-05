b2427220492463074ec9510bd4c43ba432640b27
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2022-12-05 19:20:06 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -0,0 +1,7 @@
+function solution(n) {
+    var answer = 0;
+    // console.log(Math.sqrt(n))
+    // console.log(Number.isInteger(Math.sqrt(n)))
+    if (Number.isInteger(Math.sqrt(n))) { return 1 }
+    else {return 2}
+}
