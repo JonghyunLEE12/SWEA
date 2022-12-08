9e2d5f5d3c5dbcd75f95a7fd3ff12d470a2adc4a
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2022-12-08 17:41:38 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,10 @@
+function solution(numbers, direction) {
+    var answer = [];
+    
+    if (direction === 'right') {
+        numbers.unshift(numbers.pop())
+    } else if (direction === 'left') {
+        numbers.push(numbers.shift())
+    }
+    return numbers
+}
