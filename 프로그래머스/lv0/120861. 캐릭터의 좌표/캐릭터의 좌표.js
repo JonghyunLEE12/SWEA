ab212e1b8103fc6289649fd213904def84f346ec
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2022-12-10 17:03:35 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -0,0 +1,21 @@
+function solution(keyinput, board) {
+    // 나의 위치
+    const pos = [0,0]
+    // 방향 별 변화량
+    const key = {
+        up: [0,1], 
+        down: [0,-1], 
+        left: [-1,0], 
+        right: [1,0]
+    }
+    // 최대 이동 가능거리
+    const wall = [board[0]/2 << 0 , board[1]/2 << 0]
+    keyinput.forEach(dir => {
+        // 벽을 만나지 않았다면 이동
+        if(Math.abs(pos[0]+key[dir][0]) <= wall[0] && Math.abs(pos[1]+key[dir][1]) <= wall[1]) {
+            pos[0]+=key[dir][0]
+            pos[1]+=key[dir][1]
+        }
+    })
+    return pos
+}
