a6c1ab4b34fdf36eb425af77e77aff6b985ff944
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2022-12-21 20:11:59 +0900

    [level 1] Title: 삼총사, Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,25 +1,3 @@
-// function solution(number) {
-//     let result = 0;
-
-//     const combination = (current, start) => {
-//         if (current.length === 3) {
-//             if ( current.reduce((a,b)=> a+b ,0) === 0) {
-//                 result += 1
-//             }
-//             return;
-//         }
-
-//         for (let i = start; i < number.length; i++) {
-//             combination([...current, number[i]], i + 1);
-//         }
-//     }
-//     combination([], 0);
-//     return result;
-// }
-
-
-
-
 function solution(number) {
     let result = 0;
     
