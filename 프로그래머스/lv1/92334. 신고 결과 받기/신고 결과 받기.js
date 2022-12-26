9c7104652deaeb075a07f23837e040980a27c072
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2022-12-26 18:00:46 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,29 +1,3 @@
-// def solution(id_list, report, k):
-//     answer = []
-//     report = set(report)
-//     singo_dic = dict()
-//     report_dic = dict()
-    
-//     for id in id_list:
-//         singo_dic[id] = []
-//         report_dic[id] = 0
-    
-//     for rep in report:
-//         name,report = rep.split( )
-//         singo_dic[name].append(report)
-//         report_dic[report] += 1
-    
-            
-//     for id in id_list:
-//         cnt = 0
-//         for singo in singo_dic[id]:
-//             if report_dic[singo] >= k:
-//                 cnt += 1
-//         answer.append(cnt)
-    
-        
-//     return answer
-
 function solution(id_list, report, k) {
     var answer = [];
     report = new Set(report)
@@ -35,10 +9,7 @@ function solution(id_list, report, k) {
         singoDict[id] = []
         reportDict[id] = 0
     }
-//     for rep in report:
-//         name,report = rep.split( )
-//         singo_dic[name].append(report)
-//         report_dic[report] += 1
+
     
     for (let rep of report) {
         rep = rep.split(' ')
@@ -48,12 +19,6 @@ function solution(id_list, report, k) {
         reportDict[re] += 1
     }
     
-    //     for id in id_list:
-//         cnt = 0
-//         for singo in singo_dic[id]:
-//             if report_dic[singo] >= k:
-//                 cnt += 1
-//         answer.append(cnt)
     
     for (let id of id_list) {
         let cnt = 0
