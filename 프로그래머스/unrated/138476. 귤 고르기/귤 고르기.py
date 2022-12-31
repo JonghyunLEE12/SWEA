049e6ebd7c1f4078c86142ac5b64a8ff1f903a97
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2023-01-01 00:10:18 +0900

    [unrated] Title: 귤 고르기, Time: 25.30 ms, Memory: 21.4 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -3,7 +3,7 @@ from collections import Counter
 def solution(k, tangerine):
     answer = 0
     sum = 0
-    tan = Counter(tangerine).most_common()
+    tan = Counter(tangerine).most_common() #Counter(배열).most_common() ->반복횟수확인
     
     for t in tan:
         sum += t[1]
