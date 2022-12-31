7a6c713e98a54d35f7e2c1984c38a9c0c9a7b558
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2022-12-31 12:55:31 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,7 +1,9 @@
 def solution(s):
     answer = ''
-    num = list(map(int,s.split(' ')))
+    s = s.split(' ')
+    new_s = []
+    for i in s:
+        new_s.append(int(i))
     
-    answer = f'{min(num)} {max(num)}'
-    
+    answer = str(min(new_s)) + ' ' + str(max(new_s))
     return answer
