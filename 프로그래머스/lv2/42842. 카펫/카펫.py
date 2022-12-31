4fd4ef6ffcae215a0cd87316d61a3389e438c633
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2023-01-01 00:07:28 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,10 +1,25 @@
+# def solution(brown, yellow):
+#     answer = []
+#     total = brown + yellow                  # a * b = total
+#     for b in range(1,total+1):
+#         if (total / b) % 1 == 0:            # total / b = a
+#             a = total / b
+#             if a >= b:                      # a >= b
+#                 if 2*a + 2*b == brown + 4:  # 2*a + 2*b = brown + 4 
+#                     return [a,b]
+#     return answer
+
+
 def solution(brown, yellow):
     answer = []
-    total = brown + yellow                  # a * b = total
+    total = brown + yellow
+    
     for b in range(1,total+1):
-        if (total / b) % 1 == 0:            # total / b = a
+        if (total / b) % 1 == 0:
             a = total / b
-            if a >= b:                      # a >= b
-                if 2*a + 2*b == brown + 4:  # 2*a + 2*b = brown + 4 
+            if a >= b:
+                if 2*a + 2*b == brown + 4:
                     return [a,b]
     return answer
+
+
