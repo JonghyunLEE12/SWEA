28a7fdf4860466c565a61e2396031ef844b57782
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2023-01-05 18:02:28 +0900

    [Gold V] Title: 선발 명단, Time: 192 ms, Memory: 115600 KB -BaekjoonHub

---
+++ 백준/Gold/3980. 선발 명단/선발 명단.py
@@ -0,0 +1,25 @@
+
+def dfs(player,score):
+    global max_score
+    
+    if player == 11:
+        max_score = max(max_score,score)
+        return
+        
+    for i in range(11):
+        if visited[i] or not matrix[player][i]:
+            continue
+        visited[i] = 1
+        order[player] = i
+        dfs(player+1,score+matrix[player][i])
+        order[player] = -1
+        visited[i] = 0
+
+C = int(input())
+for _ in range(C):
+    matrix = [list(map(int,input().split(' '))) for _ in range(11)]
+    order = [0] * 11
+    visited = [0] * 11
+    max_score = 0
+    dfs(0,0)
+    print(max_score)
