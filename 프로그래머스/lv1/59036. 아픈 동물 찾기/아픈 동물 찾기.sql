b8ab1c3116ae1b6e6f2fc965a1b9a2e16137917b
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2023-01-10 17:20:38 +0900

    [level 1] Title: 아픈 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
+++ 프로그래머스/lv1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -1,3 +1,6 @@
 -- 코드를 입력하세요
-SELECT animal_id, name FROM animal_ins
-WHERE intake_condition = 'Sick'
+# SELECT animal_id, name FROM animal_ins
+# WHERE intake_condition = 'Sick'
+
+SELECT ANIMAL_ID, NAME FROM ANIMAL_INS
+WHERE INTAKE_CONDITION LIKE "Sick"
