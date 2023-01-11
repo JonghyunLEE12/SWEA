d9a22aa92dd86d04969c817f7154994921ee6d01
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2023-01-11 22:59:00 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -3,7 +3,6 @@ def solution(n, lost, reserve):
     
     set_lost = set(lost) - set(reserve)
     set_reserve = set(reserve) - set(lost)
-    # 수업을 듣지 못하는 학생의 수
     cnt = 0
     for i in set_reserve:
         if i-1 in set_lost:
