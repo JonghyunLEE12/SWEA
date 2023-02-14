0175762d5b5c048cf8f1f72a28c9328de0d86750
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2023-02-14 22:38:03 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,20 +1,9 @@
-# SELECT outs.animal_id , outs.name
-# FROM animal_outs outs
-# LEFT OUTER JOIN animal_ins ins
-# ON outs.animal_id = ins.animal_id
-# WHERE ins.animal_id IS NULL
-# ORDER BY outs.animal_id
-
-
- 
-
-# SELECT A.ANIMAL_ID, A.NAME
-# FROM ANIMAL_OUTS A LEFT JOIN ANIMAL_INS B ON A.ANIMAL_ID = B.ANIMAL_ID
-# WHERE B.ANIMAL_ID IS NULL
-# ORDER BY A.ANIMAL_ID
-
-SELECT A.animal_id , A.name
-FROM ANIMAL_OUTS AS A LEFT JOIN ANIMAL_INS AS B
+SELECT A.animal_id, A.name
+FROM ANIMAL_OUTS AS A
+LEFT JOIN ANIMAL_INS AS B
 ON A.animal_id = B.animal_id
 WHERE B.animal_id IS NULL
-ORDER BY A.animal_id
+ORDER BY animal_id
+
+
+
