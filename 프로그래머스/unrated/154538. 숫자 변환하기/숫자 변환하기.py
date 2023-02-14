63c46f59ef5a5ad8d5786405365fd8b4ea4430c2
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2023-02-14 19:11:26 +0900

    [unrated] Title: 숫자 변환하기, Time: 23.01 ms, Memory: 17.5 MB -BaekjoonHub

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,25 +1,56 @@
+# from collections import deque
+
+
+# def solution(x, y, n):
+#     def bfs(x, y, n):
+#         q = deque()
+#         dist[x] = 1
+#         q.append(x)
+
+#         while q:
+#             x = q.popleft()
+#             if 0 <= x + n <= 1000000 and dist[x + n] == 0:
+#                 dist[x + n] = dist[x] + 1
+#                 q.append(x + n)
+#             if 0 <= x * 2 <= 1000000 and dist[x * 2] == 0:
+#                 dist[x * 2] = dist[x] + 1
+#                 q.append(x * 2)
+#             if 0 <= x * 3 <= 1000000 and dist[x * 3] == 0:
+#                 dist[x * 3] = dist[x] + 1
+#                 q.append(x * 3)
+
+#     dist = [0] * 1000001
+#     bfs(x,y,n)
+
+#     return dist[y]-1
+
+
 from collections import deque
 
-
-def solution(x, y, n):
-    def bfs(x, y, n):
-        q = deque()
+def solution(x,y,n):
+    
+    dist = [0]*1000001
+    def bfs(x,y,n):
+        queue = deque()
         dist[x] = 1
-        q.append(x)
-
-        while q:
-            x = q.popleft()
-            if 0 <= x + n <= 1000000 and dist[x + n] == 0:
-                dist[x + n] = dist[x] + 1
-                q.append(x + n)
-            if 0 <= x * 2 <= 1000000 and dist[x * 2] == 0:
-                dist[x * 2] = dist[x] + 1
-                q.append(x * 2)
-            if 0 <= x * 3 <= 1000000 and dist[x * 3] == 0:
-                dist[x * 3] = dist[x] + 1
-                q.append(x * 3)
-
-    dist = [0] * 1000001
+        queue.append(x)
+        
+        while queue:
+            nx = queue.popleft()
+            
+            if 0 <= nx+n <= 1000000 and dist[nx+n] == 0:
+                dist[nx+n] = dist[nx] + 1
+                queue.append(nx+n)
+            
+            if 0 <= nx*2 <= 1000000 and dist[nx*2] == 0:
+                dist[nx*2] = dist[nx] + 1
+                queue.append(nx*2)
+                
+            if 0 <= nx*3 <= 1000000 and dist[nx*3] == 0:
+                dist[nx*3] = dist[nx] + 1
+                queue.append(nx*3)
+    
     bfs(x,y,n)
-
-    return dist[y]-1
+    
+    answer = dist[y]-1
+    return answer
