9e947837e2cd0e4e21a3c66ee6e371b28b99afa6
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2023-02-15 23:49:14 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -30,10 +30,30 @@
 
 
 
-SELECT animal_ins.name , animal_ins.datetime
-FROM animal_ins
-LEFT JOIN animal_outs
-ON animal_ins.animal_id = animal_outs.animal_id
-WHERE animal_outs.animal_id IS NULL
-ORDER BY animal_ins.datetime
-limit 3
+# SELECT animal_ins.name , animal_ins.datetime
+# FROM animal_ins
+# LEFT JOIN animal_outs
+# ON animal_ins.animal_id = animal_outs.animal_id
+# WHERE animal_outs.animal_id IS NULL
+# ORDER BY animal_ins.datetime
+# limit 3
+
+
+
+
+
+
+SELECT INS.NAME, INS.DATETIME
+# SELECT *
+FROM ANIMAL_INS AS INS
+LEFT JOIN ANIMAL_OUTS AS OUTS
+ON INS.ANIMAL_ID = OUTS.ANIMAL_ID
+WHERE OUTS.ANIMAL_ID IS NULL
+ORDER BY INS.DATETIME
+LIMIT 3
+
+
+
+
+
+
