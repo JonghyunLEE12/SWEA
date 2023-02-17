84913fb402ca321bc4e1b3bc54c56ff51ee9fbe2
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2023-02-17 22:16:04 +0900

    [Silver II] Title: 숫자판 점프, Time: 280 ms, Memory: 116364 KB -BaekjoonHub

--- 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
@@ -1,31 +1,62 @@
-# DFS로 특정 노드를 방문하고 연결된 모든 노드들도 방문
-def dfs(x, y, number):
-    number += str(graph[x][y])
+'''
+1 1 1 1 1
+1 1 1 1 1
+1 1 1 1 1
+1 1 1 2 1
+1 1 1 1 1
+'''
 
-    # 숫자 길이가 6이면 멈추고 똑같은 숫자의 합이 있는지 결과 리스트에서 확인
+
+
+matrix = [ list(map(int,input().split(' '))) for _ in range(5)]
+result = []
+
+# 델타 상 하 좌 우
+dr = [-1,1,0,0]
+dc = [0,0,-1,1]
+
+
+# def dfs(row,col,number):
+#     # number += str(matrix[row][col])
+#     number += str(matrix[row][col])
+
+
+#     if len(number) == 6:
+#         if number not in result:
+#             result.append(number)
+#         return
+    
+#     for k in range(len(dr)):
+#         nr = row + dr[k]
+#         nc = col + dc[k]
+
+#         if 0 <= nc < 5 and 0 <= nc < 5:
+#             dfs(nr,nc,number)
+
+
+
+# DFS로 특정 노드를 방문하고 연결된 모든 노드들도 방문
+def dfs(row, col, number):
+    number += str(matrix[row][col])
+
     if len(number) == 6:
         if number not in result:
             result.append(number)
         return
+    
 
     # 좌/우/위/아래 방향의 좌표를 탐색
-    for i in range(4):
-        a = x + dx[i]
-        b = y + dy[i]
+    for i in range(len(dr)):
+        nr = row + dr[i]
+        nc = col + dc[i]
 
         # 맵 정보를 넘지 않는다면 탐색
-        if 0 <= a < 5 and 0 <= b < 5:
-            dfs(a, b, number)
-
-
-# 2차원 맵 정보를 입력
-graph = [list(map(int,input().split(' '))) for _ in range(5)]
-dy = [0, 0, 1, -1]
-dx = [1, -1, 0, 0]
-result = []
+        if 0 <= nr < 5 and 0 <= nc < 5:
+            dfs(nr,nc, number)
 
 for i in range(5):
     for j in range(5):
-        dfs(i, j, "")
+        dfs(i,j,'')
+
 
 print(len(result))
