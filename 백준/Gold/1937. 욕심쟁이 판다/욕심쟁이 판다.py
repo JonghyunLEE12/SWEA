646f04608a8b399f54e936dddf253613d4463999
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2023-03-19 17:20:48 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 884 ms, Memory: 256612 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -1,5 +1,5 @@
 n = int(input())
-matrix = [ list(map(int,input().split(' '))) for _ in range(n)]
+matrix = [list(map(int,input().split(' '))) for _ in range(n)]
 answer = 0
 
 # 델타 상 하 좌 우
@@ -8,20 +8,21 @@ dc = [0,0,-1,1]
 
 dp = [[0]*n for _ in range(n)]
 
-
 def dfs(r,c):
     if dp[r][c]: return dp[r][c]
-
     dp[r][c] = 1
+
     for i in range(len(dr)):
         nr = r + dr[i]
         nc = c + dc[i]
 
         if 0 <= nr < n and 0 <= nc < n:
             if matrix[nr][nc] > matrix[r][c]:
-                dp[r][c] = max(dp[r][c] , dfs(nr,nc)+1)
+                dp[r][c] = max(dp[r][c], dfs(nr,nc)+1)
 
     return dp[r][c]
+
+
 
 for i in range(n):
     for j in range(n):
