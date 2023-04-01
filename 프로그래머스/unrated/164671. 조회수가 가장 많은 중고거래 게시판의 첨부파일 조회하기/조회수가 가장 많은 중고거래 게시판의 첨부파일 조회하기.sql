fec8c52f3704b7300807a15c5726d27a8189c78f
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2023-04-01 17:41:18 +0900

    [unrated] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -7,12 +7,24 @@
 # WHERE BOARD.VIEWS = ( SELECT MAX(b1.views) FROM USED_GOODS_BOARD b1)
 # ORDER BY FILE.FILE_ID DESC
 
-SELECT concat('/home/grep/src/',f.board_id,'/',f.file_id, f.file_name, f.file_ext) as file_path
-from used_goods_file f 
-join used_goods_board b
-on f.board_id = b.board_id
-where b.views = (select max(b1.views) from used_goods_board b1 )
-order by f.file_id desc
+# SELECT concat('/home/grep/src/',f.board_id,'/',f.file_id, f.file_name, f.file_ext) as file_path
+# from used_goods_file f 
+# join used_goods_board b
+# on f.board_id = b.board_id
+# where b.views = (select max(b1.views) from used_goods_board b1 )
+# order by f.file_id desc
+
+
+SELECT 
+CONCAT('/home/grep/src/',FL.BOARD_ID,'/',FL.FILE_ID,FL.FILE_NAME,FL.FILE_EXT)
+AS FILE_PATH
+FROM USED_GOODS_FILE AS FL
+JOIN USED_GOODS_BOARD AS BD
+ON FL.BOARD_ID = BD.BOARD_ID
+WHERE BD.VIEWS = (SELECT MAX(B1.VIEWS) FROM USED_GOODS_BOARD B1)
+ORDER BY FL.FILE_ID DESC
+
+
 
 
 
