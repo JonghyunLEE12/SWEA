1bb8e3d95b56a11d256fb6aaab9d93cb44648e05
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2023-05-11 19:53:10 +0900

    [unrated] Title: rny_string, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.py
@@ -0,0 +1,9 @@
+def solution(rny_string):
+    answer = ''
+    
+    for word in rny_string:
+        if word == 'm':
+            answer += 'rn'
+        else:
+            answer += word
+    return answer
