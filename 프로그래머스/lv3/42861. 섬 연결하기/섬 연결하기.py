a5435cef47463ac2e2c8352f48a7ee1dbed1c133
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2023-06-06 02:28:32 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,21 +1,57 @@
 def solution(n, costs):
     answer = 0
-    uni = [0] * (n + 1)
-    costs.sort(key = lambda x: x[2])
-    for i in range(1,n + 1):
-        uni[i] = i
-    def union(x,y):
-        a = find(x)
-        b = find(y)
-        if a != b:
-            uni[a] = b
-    def find(x):
-        if uni[x] == x: return x
+    
+    parents = [i for i in range(n+1)]
+    costs.sort(key= lambda x : x[2])
+    
+    def find(a):
+        if parents[a] == a:
+            return a
         else:
-            uni[x] = find(uni[x])
-            return uni[x]
+            b = find(parents[a])
+            parents[a] = b
+            return b
+    
+    def union(a,b):
+        a = find(a)
+        b = find(b)
+        
+        if a != b:
+            parents[a] = b
+    
+    
     for n1,n2,cost in costs:
         if find(n1) != find(n2):
             union(n1,n2)
             answer += cost
     return answer
+
+
+
+# def solution(n, costs):
+#     answer = 0
+#     uni = [0] * (n + 1)
+#     costs.sort(key = lambda x: x[2])
+#     for i in range(0,n + 1):
+#         uni[i] = i
+        
+#     def union(x,y):
+#         a = find(x)
+#         b = find(y)
+#         if a != b:
+#             uni[a] = b
+            
+            
+#     def find(x):
+#         if uni[x] == x: return x
+#         else:
+#             uni[x] = find(uni[x])
+#             return uni[x]
+    
+    
+#     for n1,n2,cost in costs:
+#         if find(n1) != find(n2):
+#             union(n1,n2)
+#             answer += cost
+            
+#     return answer
