079291ebfc57ee9a4166271955de83f77b7ed329
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2023-06-14 20:58:17 +0900

    [Silver III] Title: 달팽이, Time: 332 ms, Memory: 132488 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.py
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -1,33 +1,52 @@
-import  copy
+'''
+7
+35
+'''
 
 n = int(input())
-m = int(input())
-snail = []
-temp = []
+target = int(input())
 
-for i in range(n):
-    temp.append(0)
+matrix = [[0] * n for _ in range(n)]
+visited = []
 
-for i in range(n):
-    snail.append(copy.deepcopy(temp))
+for mat in matrix:
+    visited.append(mat[::])
 
-move = [[0,1],[1,0],[0,-1],[-1,0]]
-d = 0
-now_x = 0 ; now_y = 0 ; now_num = n*n
+# 델타 하 우 상 좌
+dr = [1,0,-1,0]
+dc = [0,1,0,-1]
 
-while(now_num > 0):
-    snail[now_y][now_x] = now_num
-    if(now_x + move[d][0] < 0 or now_x + move[d][0] >= n or now_y + move[d][1] < 0 or now_y + move[d][1] >= n or snail[now_y+move[d][1]][now_x+move[d][0]] != 0):
-        d = (d + 1) % 4
-    now_y = now_y + move[d][1]
-    now_x = now_x + move[d][0]
-    now_num = now_num - 1
 
-for i in range(n):
-    for j in range(n):
-        if(snail[i][j] == m):
-            find_x = j
-            find_y = i
-        print(snail[i][j] , end = ' ')
-    print()
-print(find_y + 1,find_x + 1)
+direction = 0
+
+row = 0
+col = 0
+number = n*n
+
+
+
+while True:
+    if number == 0:
+        break
+
+    matrix[row][col] = number
+
+    if number == target:
+        ans = [row+1,col+1]
+    
+    nr = row + dr[direction]
+    nc = col + dc[direction]
+
+    if nr < 0 or nc < 0 or nr >= n or nc >= n or matrix[nr][nc] != 0:
+        direction = (direction + 1) % 4
+
+    row = row + dr[direction]
+    col = col + dc[direction]
+
+    number -= 1
+
+
+for mat in matrix:
+    print(*mat)
+
+print(*ans)
