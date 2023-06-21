50319ad6be681ff90aacaf58ec278a9463af0213
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2023-06-21 15:41:01 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/lv2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,7 +1,12 @@
 -- 코드를 입력하세요
- SELECT animal_type , IFNULL(name,"No name") AS name , sex_upon_intake
- from animal_ins
+#SELECT animal_type , IFNULL(name,"No name") AS name , sex_upon_intake
+#from animal_ins
+
+# SELECT ANIMAL_TYPE, IFNULL(NAME,"No name") AS NAME, SEX_UPON_INTAKE
+# FROM ANIMAL_INS
 
 
-#SELECT ANIMAL_TYPE, IFNULL(NAME,"NO name") AS NAME, SEX_UPON_INTAKE
-#FROM ANIMAL_INS
+
+
+SELECT ANIMAL_TYPE, IFNULL(NAME,"No name") AS NAME , SEX_UPON_INTAKE
+FROM ANIMAL_INS
