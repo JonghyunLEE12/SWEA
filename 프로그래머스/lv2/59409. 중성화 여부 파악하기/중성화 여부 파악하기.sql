ac1ff6ed4190da98f7e3acbd533881bece84fe01
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2023-06-30 19:45:41 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -6,8 +6,33 @@
 
 
 
-SELECT animal_id , name ,
-IF ( sex_upon_intake LIKE '%Neutered%' OR sex_upon_intake LIKE '%Spayed%','O','X')
-AS '중성화'
-FROM animal_ins
-ORDER BY animal_id
+# SELECT animal_id , name ,
+# IF ( sex_upon_intake LIKE '%Neutered%' OR sex_upon_intake LIKE '%Spayed%','O','X')
+# AS '중성화'
+# FROM animal_ins
+# ORDER BY animal_id
+
+
+
+SELECT ANIMAL_ID,NAME,(
+    CASE
+        WHEN ANIMAL_ID IN (
+            SELECT ANIMAL_ID
+            FROM ANIMAL_INS
+            WHERE SUBSTR(SEX_UPON_INTAKE,1,1) LIKE 'N' OR
+            SUBSTR(SEX_UPON_INTAKE,1,1) LIKE 'S'
+        ) THEN 'O'
+        ELSE 'X'
+    END
+) AS '중성화'
+
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
+
+
+
+
+
+
+
+
