538b3b7afd37a4f7fd4879282cead09bd688282b
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2023-07-02 21:19:05 +0900

    [level 2] Title: 이름에 el이 들어가는 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++ 프로그래머스/lv2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -1,9 +1,9 @@
 -- 코드를 입력하세요
-# SELECT animal_id , name from animal_ins
-# where animal_type = "Dog" and name like "%el%"
-# order by name
+SELECT animal_id , name from animal_ins
+where animal_type = "Dog" and name like "%el%"
+order by name
 
-SELECT ANIMAL_ID,NAME
-FROM ANIMAL_INS
-WHERE ANIMAL_TYPE = 'DOG' AND NAME LIKE "%el%"
-ORDER BY NAME
+# SELECT ANIMAL_ID,NAME
+# FROM ANIMAL_INS
+# WHERE ANIMAL_TYPE = 'DOG' AND NAME LIKE "%el%"
+# ORDER BY NAME
