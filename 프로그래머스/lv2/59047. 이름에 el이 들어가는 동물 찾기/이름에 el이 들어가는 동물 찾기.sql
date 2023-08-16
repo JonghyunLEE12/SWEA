fb602a5561a3fc54ce0b57ef011e06dcb590964b
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2023-08-16 19:57:28 +0900

    [level 2] Title: 이름에 el이 들어가는 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++ 프로그래머스/lv2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -3,6 +3,12 @@
 # where animal_type = "Dog" and name like "%el%"
 # order by name
 
+# SELECT ANIMAL_ID,NAME
+# FROM ANIMAL_INS
+# WHERE ANIMAL_TYPE = 'DOG' AND NAME LIKE "%el%"
+# ORDER BY NAME
+
+
 SELECT ANIMAL_ID,NAME
 FROM ANIMAL_INS
 WHERE ANIMAL_TYPE = 'DOG' AND NAME LIKE "%el%"
