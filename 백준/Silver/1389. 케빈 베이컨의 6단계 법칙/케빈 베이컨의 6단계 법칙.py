8c1c04c5d5588a24d8a0d11aaf5f0fbf699e16e5
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2023-09-19 22:45:17 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 132 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+INF = int(1e9)
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+# 플로이드 워셜 알고리즘
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+# 각 사람의 케빈 베이컨 수 계산
+bacon = [0] * (n + 1)
+for i in range(1, n + 1):
+    bacon[i] = sum(graph[i][1:])
+
+print(bacon.index(min(bacon[1:])))
