03bdba1993621bd8b4fb22616033d3908f10515a
이종현 <59881536+JonghyunLEE12@users.noreply.github.com>
2024-01-13 14:43:18 +0900

    [Silver I] Title: 미로 탐색, Time: 152 ms, Memory: 114868 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -8,11 +8,10 @@
 
 from collections import deque
 
+
 n,m = map(int,input().split(' '))
-
-matrix = [list(map(str,input().rstrip(' '))) for _ in range(n)]
+matrix = [ list(map(str,input().rstrip(' '))) for _ in range(n) ]
 visited = [[0]*m for _ in range(n)]
-
 
 # 델타 상 하 좌 우
 dr = [-1,1,0,0]
@@ -25,7 +24,6 @@ def bfs(r,c):
 
     while queue:
         row,col = queue.popleft()
-
         for i in range(len(dr)):
             nr = row + dr[i]
             nc = col + dc[i]
@@ -34,8 +32,6 @@ def bfs(r,c):
                 if visited[nr][nc] == 0 and matrix[nr][nc] == '1':
                     queue.append([nr,nc])
                     visited[nr][nc] = visited[row][col] + 1
+    return visited[n-1][m-1]
 
-    
-
-bfs(0,0)
-print(visited[n-1][m-1])
+print(bfs(0,0))
